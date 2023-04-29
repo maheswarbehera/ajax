8990158e43993ada93574f28350cfdf67963b9a1
maheswar behera <bmaheswar439@gmail.com>
2023-04-29 08:25:31 +0530

    database chnage

--- admin/db.php
+++ admin/db.php
@@ -1,11 +1,11 @@
 <?php
-    $servername ="localhost";
+    $servername ="localhost:9090";
     $username ="root";
     $password ="";
-    $database ="admin";   
+    $database ="api";   
 
     //create connection
-    $conn = mysqli_connect($servername, $username, $password, $database);
+    $con = mysqli_connect($servername, $username, $password, $database);
 
     //check connection
     if(!$con){
